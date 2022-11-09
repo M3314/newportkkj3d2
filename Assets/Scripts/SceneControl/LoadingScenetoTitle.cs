7de05aa510897bb14e59f4b36f505f979238ef68
M3314 <srapid670@gmail.com>
2022-11-10 02:39:23 +0900

    잠시 수정

--- Assets/Scripts/SceneControl/LoadingScenetoTitle.cs
+++ Assets/Scripts/SceneControl/LoadingScenetoTitle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5badc9b476dcfcff48058b9f6811d38c1f9f53bad5a616f8ba99780433b34123
-size 1911
+oid sha256:de25c201bad281cafa34ad42f916f67a1f9cb6bcde7bd6f198388d462811f2e3
+size 1917
--- Assets/Scripts/UI Scene/FadeText.cs
+++ Assets/Scripts/UI Scene/FadeText.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ea2325abdde9b791eace71687324e201f0aa8ef6901f3c86e063a547601634
-size 1163
+oid sha256:2160b204cf913d242170de6bbdf6ee37032ad280c69f392c0a01db273de34d46
+size 1167
