b3b8e079b73fa2b99e74486827bbb1639cac2e04
M3314 <srapid670@gmail.com>
2022-12-30 22:50:57 +0900

    HP 연동
    몬스터 HP 연동 (단 Vector3 부분을 해결 해야됨, 왜 자꾸 플레이어가 붙어버리면 고정되는지 이해 불가)

--- Assets/Scripts/Enemy/SkeletonMoving.cs
+++ Assets/Scripts/Enemy/SkeletonMoving.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7aa717f5f863fdc57ae5faa7efdb539832ba0c1a154f4e66c1db914771295b9
-size 3484
+oid sha256:aafefa33f2b7ded21f2ea91f6f9c0bea124fbb23b336b004a7587d32d2b2ad98
+size 4972
---
+++ Assets/Scripts/UI Scene/SkeletonHP.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad5c29bbe9f6337e7c91254863f0077f85f1ea7a6597eb401c1ed02d5a94f3c7
+size 961
